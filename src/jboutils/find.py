6980206f93e4ef1e7694d44d8d2c4f6e74ffe87b
James Broadhead <jamesbroadhead@gmail.com>
2020-02-26 17:22:23 +0000

    find: add the ability to pass filenames or iterables

--- src/jboutils/find.py
+++ src/jboutils/find.py
@@ -1,11 +1,10 @@
-""" jboutils.find """
-
 import os
 from os.path import isfile, islink
 import fnmatch
 
+from collections.abc import Iterable
 
-def find(top_dir, pattern=None, filters=None):
+def find(target, pattern=None, filters=None, _norecurse=False):
     """
     Based on:
     http://www.dabeaz.com/generators-uk/genfind.py
@@ -18,6 +17,30 @@ def find(top_dir, pattern=None, filters=None):
     if filters is None:
         filters = []
 
+    if isinstance(target, str):
+        # a file or dir
+        if isfile(target):
+            return _find_file(target, pattern, filters) # returns a generator of strings
+        return _find_dir(target, pattern, filters) # returns a generator of strings
+
+    elif isinstance(target, Iterable):
+        if _norecurse:
+            raise ValueError('This method only support iterables of paths, not iterables of iterables')
+        # returns a list of generators of strings or
+        generators = [ find(item, pattern, filters, _norecurse=True) for item in target ]
+
+        # flatMap - return a generator of strings
+        for generator in generators:
+            for item in generator:
+                yield item
+
+    raise ValueError('Must pass either a path or an iterable of paths, got {} which is a {}'.format(target, type(target)))
+
+def _find_file(filepath, pattern, filters):
+    if fnmatch.filter([filepath], pattern) and all([f(filepath) for f in filters]):
+        yield filepath
+
+def _find_dir(top_dir, pattern, filters):
     for path, _, filelist in os.walk(top_dir):
         for name in fnmatch.filter(filelist, pattern):
             full_path = os.path.join(path, name)
